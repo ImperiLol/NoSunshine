be00bc2e53cdfcd32d7e2e7c8e7931827bc274ed
ImperiLol <ImperiYT1@gmail.com>
2026-09-23 11:49:47 -0700

    fixed it

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,49 +1,31 @@
-public class NoSunshine
-{
-    public static void gone()
-    {
-        System.out.println("Ain't no sunshine when she's gone");
+public class NoSunshine {
+    static void g(){System.out.println("Ain't no sunshine when she's gone");}
+    static void a(){System.out.println("Anytime she goes away");}
+
+    static void v1(String l1, String l2){
+        g(); System.out.println(l1); g(); System.out.println(l2); a(); System.out.println();
     }
 
-    public static void away()
-    {
-        System.out.println("Anytime she goes away");
+    static void v2(){
+        System.out.println("Wonder this time where she's gone\nWonder if she's gone to stay");
+        g();
+        System.out.println("And this house just ain't no home");
+        a(); System.out.println();
+    }
+    
+    static void k(){
+       System.out.print("And "+"I know, ".repeat(4).trim()+"\n"+("I know, ".repeat(5).trim()+"\n").repeat(4)+"I know, I know\n\n");
     }
 
-    public static void verse(String uniqueLine1, String uniqueLine2)
-    {
-        gone();
-        System.out.println(uniqueLine1);
-        gone();
-        System.out.println(uniqueLine2);
-        away();
-        System.out.println();
-    }
-
-      public static void know() {
-         System.out.print("And " + "I know, I know, I know, I know\n".repeat(5) 
-            + "I know, I know\n\n");
-    }
-
-
-    public static void main(String[] args)
-    {
-        verse("It's not warm when she's away", "And she's always gone too long");
-        verse("Wonder this time where she's gone", "Wonder if she's gone to stay");
-        
-        know();
-
-        System.out.println("Hey, I ought to leave young thing alone");
-        System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
-        gone();
+    public static void main(String[] args){
+        v1("It's not warm when she's away", "And she's always gone too long");
+        v2();
+        k();
+        System.out.println("Hey, I ought to leave young thing alone\nBut ain't no sunshine when she's gone, whoa-whoa");
+        g();
         System.out.println("Only darkness every day\n");
-
-        gone();
+        g();
         System.out.println("And this house just ain't no home\n");
-        
-        away();
-        away();
-        away();
-        away();
+        a(); a(); a(); a();
     }
 }
